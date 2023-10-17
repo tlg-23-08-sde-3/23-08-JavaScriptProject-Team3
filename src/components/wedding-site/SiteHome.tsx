39690dc5598aec059c296479eb1b3a9e7b146b04
veresgv <genevieve.veres@tlglearning.com>
2023-10-17 16:53:38 -0400

    comment submit handler, css corrections

--- src/components/wedding-site/SiteHome.tsx
+++ src/components/wedding-site/SiteHome.tsx
@@ -3,11 +3,11 @@ import "./SiteHome.css";
 export const SiteHome = () => {
     return (
         <>
-            <h2>Our Love Story</h2>
+            <h2 className="sitehome-h2">Our Love Story</h2>
             <p className="storyContainer">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro neque incidunt ut fugiat corporis commodi dicta architecto voluptatem nemo. Commodi perspiciatis animi ipsam laboriosam doloribus aliquid assumenda fugiat illum. Molestiae.</p>
-            <h2>Wedding Date:</h2>
+            <h2 className="sitehome-h2">Wedding Date:</h2>
             <h4 className="dateContainer">February 30th, 2020</h4>
-            <h2>Venue:</h2>
+            <h2 className="sitehome-h2">Venue:</h2>
             <h4 className="">The Heavenly Dominion</h4>
         </>
     );
