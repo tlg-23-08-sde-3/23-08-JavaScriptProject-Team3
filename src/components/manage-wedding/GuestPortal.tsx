63a5c7cb6845179f8f73da6211828c88eb2185b1
Chandan Chauhan <bellicose100xp@users.noreply.github.com>
2023-10-17 21:04:49 -0700

    Wedding Portal Minor fixes

--- src/components/manage-wedding/GuestPortal.tsx
+++ src/components/manage-wedding/GuestPortal.tsx
@@ -1,9 +1,24 @@
-import "./GuestPortal.css"
+import "./GuestPortal.css";
+
+const data = {
+    id1: {
+        name: "Hugh Jackman",
+        rsvp: "Attending",
+        plusOne: "yes",
+    },
+    id2: {
+        name: "John Doe",
+        rsvp: "Pending",
+        plusOne: "no",
+    },
+};
+
+console.log(data);
 
 export const GuestPortal = () => {
     return (
-        <>
-        <h1>Guest Portal</h1>
-        </>
+        <div className="guest-portal-container">
+            <h1 className="guest-portal-heading">Guest Portal</h1>
+        </div>
     );
 };
