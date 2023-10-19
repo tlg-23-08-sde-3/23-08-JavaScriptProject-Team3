3cca4121aa3cb2492931eec130d6758576efa369
Chandan Chauhan <bellicose100xp@users.noreply.github.com>
2023-10-18 21:47:33 -0700

    Homepage in progress

--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Homepage.css";
 
 export const Homepage = () => {
@@ -13,14 +14,47 @@ export const Homepage = () => {
                 <div className="homepage-card-details">
                     <p>
                         With so much to do before your wedding day, you need a wedding website that
-                        gets things done. See how a Elegant Weddings website can simplify your wedding
-                        planning.
+                        gets things done. See how an <strong>Elegant Weddings</strong> website can
+                        simplify your wedding planning.{" "}
+                        <Link className="link-secondary" to="/signup">
+                            Sign Up
+                        </Link>{" "}
+                        to get started.
+                        <p style={{ marginTop: "0.5em" }}>
+                            Already have an account{" "}
+                            <Link className="link-secondary" to="/login">
+                                Login Here
+                            </Link>
+                        </p>
                     </p>
+
                     <div className="homepage-card-image">
                         <img src="website_builder.jpg" alt="all in one website builder" />
                     </div>
                 </div>
             </div>
+
+            <div className="features-container">
+                <div className="features-image">
+                    <img src="./rsvp.jpg"></img>
+                    <h1>RSVP Management</h1>
+                </div>
+                <div className="features-image">
+                    <img src="./registry.jpg"></img>
+                    <h1>Wedding Registry</h1>
+                </div>
+                <div className="features-image">
+                    <img src="./gallery.jpg"></img>
+                    <h1>
+                        Photo
+                        <br /> Gallery
+                    </h1>
+                </div>
+            </div>
+
+            <footer className="homepage-footer">
+                copyright © 
+            </footer>
         </div>
     );
 };
