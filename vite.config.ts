5f214da59722f0f0d53a330baee285ead51820f6
Chandan Chauhan <bellicose100xp@users.noreply.github.com>
2023-10-19 13:49:31 -0700

    changed based deploy folder in vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/23-08-JavaScriptProject-Team3/",
+    base: "/23-08-JavaScriptProject-Team3/dist/",
     plugins: [react()],
 });
