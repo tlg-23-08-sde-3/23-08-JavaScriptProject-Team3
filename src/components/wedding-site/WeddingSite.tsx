5577abcd7f07f1dca0e0e8a560cd0830742bdb5c
Chandan Chauhan <bellicose100xp@users.noreply.github.com>
2023-10-19 11:25:45 -0700

    Updated menu on Wedding Page

--- src/components/wedding-site/WeddingSite.tsx
+++ src/components/wedding-site/WeddingSite.tsx
@@ -1,7 +1,6 @@
 import "./WeddingSite.css";
 import { useParams, NavLink, Outlet } from "react-router-dom";
 
-
 export const WeddingSite = () => {
     const { id } = useParams();
 
@@ -11,15 +10,24 @@ export const WeddingSite = () => {
                 <h1>Wedding Page for {id}</h1>
             </div>
             <div className="wedding-nav-items">
-                <div className="wedding-nav-item"><NavLink to={`/site/${id}`}>Home </NavLink></div>
-                <div className="wedding-nav-item"><NavLink to={`/site/${id}/photos`}>Photos </NavLink></div>
-                <div className="wedding-nav-item"><NavLink to={`/site/${id}/registry`}>Registry </NavLink></div>
-                <div className="wedding-nav-item"><NavLink to={`/site/${id}/rsvp`}>RSVP </NavLink></div>
-                <div className="wedding-nav-item"><NavLink to={`/site/${id}/guestbook`}>Guest Book </NavLink></div>
+                <div className="wedding-nav-item">
+                    <NavLink to={`/site/${id}`} end>Home </NavLink>
+                </div>
+                <div className="wedding-nav-item">
+                    <NavLink to={`/site/${id}/photos`}>Photos </NavLink>
+                </div>
+                <div className="wedding-nav-item">
+                    <NavLink to={`/site/${id}/registry`}>Registry </NavLink>
+                </div>
+                <div className="wedding-nav-item">
+                    <NavLink to={`/site/${id}/rsvp`}>RSVP </NavLink>
+                </div>
+                <div className="wedding-nav-item">
+                    <NavLink to={`/site/${id}/guestbook`}>Guest Book </NavLink>
+                </div>
             </div>
 
-            <Outlet context={id}/>
-
+            <Outlet context={id} />
         </div>
     );
 };
