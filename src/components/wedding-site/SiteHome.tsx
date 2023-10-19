d8bcbd569387376081a122b30e1035f5ae772458
Genevieve Veres <Genevieve.Veres@tlgcohort.com>
2023-10-19 14:02:18 -0400

    Styling changes

--- src/components/wedding-site/SiteHome.tsx
+++ src/components/wedding-site/SiteHome.tsx
@@ -20,12 +20,12 @@ export const SiteHome = () => {
 
     return (
         <>
-            <h2 className="sitehome-h2">Our Love Story</h2>
+            <h2 className="sitehome-h2">💗Our Love Story💗</h2>
             <p className="storyContainer">{siteHomeState.story}</p>
-            <h2 className="sitehome-h2">Wedding Date:</h2>
-            <h4 className="dateContainer">{siteHomeState.weddate}</h4>
-            <h2 className="sitehome-h2">Venue:</h2>
-            <h4 className="">{siteHomeState.venue}</h4>
+            <h2 className="sitehome-h2">📅Wedding Date:📅</h2>
+            <p className="dateContainer">{siteHomeState.weddate}</p>
+            <h2 className="sitehome-h2">💒Venue:💒</h2>
+            <p className="venueContainer">{siteHomeState.venue}</p>
         </>
     );
 };
